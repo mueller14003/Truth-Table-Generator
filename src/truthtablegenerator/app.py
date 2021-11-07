8a0ce1e9ed50b63955f0fffb31e6870f590a652b
mueller14003 <kyle.mueller.ghs@gmail.com>
2021-11-06 21:03:55 -0600

    Code style and documentation improvements, some performance optimizations.

--- src/truthtablegenerator/app.py
+++ src/truthtablegenerator/app.py
@@ -22,32 +22,73 @@ class Infix(object):
     def __call__(self, v1, v2):
         return self.func(v1, v2)
 
+# Valid symbols that are not alphabetical characters
 valid_symbols = ['→','↔','∨','∧','⊕','¬','(',')']
 
 #         ~   ¬     ^   ⊕     &    ∧     |    ∨
 t_dict = {38: 8743, 94: 8853, 124: 8744, 126: 172} # Dict for str.translate()
-prettify = lambda s: " ".join(s.translate(t_dict).replace('<->','↔').replace('->','→').replace(" ",""))
 
-cond = Infix(lambda a,b: ~a | b) # Conditional
-bicond = Infix(lambda a,b: ~(a ^ b)) # Biconditional
-fix_inline = lambda s: s.replace('<->','|bicond|').replace('->','|cond|')
+# Prettifies and input string by using more visually appealing symbols and equalizing spacing
+def prettify(s): 
+    return " ".join(s.translate(t_dict)
+                     .replace('<->','↔')
+                     .replace('->','→')
+                     .replace(" ",""))
 
-flatten = lambda l: sum(map(flatten,l),[]) if isinstance(l,list) else [l] # Flattens lists
-get_arity = lambda f: len(signature(f).parameters) # Returns Arity of a Function
-get_bits = lambda n: [*map(lambda x:[*map(int,bin(x)[2:].zfill(n))],range(2**n))] # Get Bits
-get_string_bits = lambda n: [*map(lambda x:[*map(str,bin(x)[2:].zfill(n))],range(2**n))] # Get String Bits
-gbfa = lambda f: get_bits(get_arity(f)) # Get Bits from Arity
-gsbfa = lambda f: get_string_bits(get_arity(f)) # Get String Bits from Arity
-output = lambda f: [*map(f,*zip(*gbfa(f)))] # Output of Boolean Function
-make_tt = lambda f: [*map(flatten,[*map(list,zip(gsbfa(f),output(f)))])] # Make Truth Table
-gv = lambda s: sorted([*{*filter(str.isalpha,s)}]) # Gets Variables from String
-gvf = lambda s: ','.join(gv(s)) # Get Vars (formatted)
-make_truth_table = lambda s: make_tt(eval(f"lambda {gvf(s)}:['0','1'][{fix_inline(s)}]")) # Make Truth Table from String
-get_headings = lambda s: [*gv(s),prettify(s)]
+conditional = Infix(lambda a,b: ~a | b)
+biconditional = Infix(lambda a,b: ~(a ^ b))
+
+# Makes `->` and `<->` inline functions
+def fix_inline(s):
+    return (s.replace('<->','|biconditional|')
+             .replace('->','|conditional|'))
+
+# Flattens lists
+def flatten(l): 
+    if isinstance(l,list):
+        return sum(map(flatten,l),[])
+    return [l]
+
+# Takes a function as an input and returns the arity of the function
+def get_arity(f): 
+    return len(signature(f).parameters)
+
+# Gets the input columns for the truth table as strings
+def get_bits(n): 
+    return [*map(lambda x:[*map(str,bin(x)[2:].zfill(n))],range(2**n))]
+
+# Given an input function, use the arity to get the input columns for the truth table
+def get_bits_f(f): 
+    return get_bits(get_arity(f))
+
+# Gets the output column of the truth table
+def output(f): 
+    return [*map(f,*zip(*get_bits_f(f)))]
+
+# Combines the input columns of the truth table with the output column
+def make_tt(f): 
+    return [*map(flatten,[*map(list,zip(get_bits_f(f),output(f)))])]
+
+# Gets the input variables for the boolean expression
+def get_vars(s):
+    return sorted([*{*filter(str.isalpha,s)}])
+
+# Gets the parameters to be used in the generated function
+def get_parameters(s):
+    return lambda s: ','.join(get_vars(s))
+
+# Takes in a boolean expression as a string, converts it into a function, and makes the truth table
+def make_truth_table(s):
+    return make_tt(eval(f"lambda {get_parameters(s)}:['0','1'][{fix_inline(s)}]"))
+
+# Gets the headings to display the truth table
+def get_headings(s): 
+    return [*get_vars(s),prettify(s)]
 
 
 # HTML Function
 
+# Generates the truth table string to be saved in an HTML file
 def make_tt_html(s):
     return f"""<!DOCTYPE html>
 <html>
@@ -84,44 +125,87 @@ code {chr(123)}
 
 # Markdown Functions
 
-md_line = lambda l: f"|{'|'.join(map(str,l))}|" # Markdown Line
-md_header = lambda s: f"|{'|'.join(gv(s))}|{prettify(s)}|\n|{'|'.join('-'*len(gv(s)))}|:-:|\n" # Markdown Header
-md_table = lambda s: md_header(s)+'\n'.join(map(md_line,make_truth_table(s))) # Markdown Truth Table
-make_tt_markdown = lambda s: f"# Truth Table for `{prettify(s)}`\n{md_table(s)}"
+# Generates a Markdown-formatted line
+def md_line(l): 
+    return f"|{'|'.join(map(str,l))}|"
+
+# Generates a Markdown-formatted header
+def md_header(s):
+    vars_ = get_vars(s)
+    return f"|{'|'.join(vars_)}|{prettify(s)}|\n|{'|'.join('-'*len(vars_))}|:-:|\n"
+
+# Generates a Markdown-formatted table
+def md_table(s):
+    return md_header(s)+'\n'.join(map(md_line,make_truth_table(s)))
+
+# Generates the truth table string to be saved in a Markdown file
+def make_tt_markdown(s):
+    return f"# Truth Table for `{prettify(s)}`\n{md_table(s)}"
 
 
 # LaTeX Functions
 
-lt_prettify = lambda s: prettify(s).replace(
-    '→','\\rightarrow').replace(
-    '↔','\\leftrightarrow').replace(
-    '∨','\\lor').replace(
-    '∧', '\\land').replace(
-    '⊕', '\\oplus').replace(
-    '¬', '\\neg')
-lt_line = lambda l: ' & '.join(map(str,l))+'\\\\'
-lt_header = lambda s: lt_line(gv(s)+[lt_prettify(s)])
-lt_table = lambda s: '\n'.join(map(lt_line,make_truth_table(s)))
-make_tt_latex = lambda s: "\\documentclass[a4paper]{article}\n\\begin{document}" + \
-    "\n\\title{Truth Table for $" + lt_prettify(s) + "$}\n\\author{Kyle Mueller}" + \
-    "\n\\maketitle\n\\begin{displaymath}" + \
-    "\n\\begin{array}{|" + ' '.join('c'*len(gv(s))) + "|c|}\n" + lt_header(s) + \
-    "\n\\hline\n" + lt_table(s) + \
-    "\n\\end{array}\n\\end{displaymath}\n\\end{document}"
+# Converts prettified string to LaTeX style
+def lt_prettify(s): 
+    return prettify(s).replace(
+        '→','\\rightarrow').replace(
+        '↔','\\leftrightarrow').replace(
+        '∨','\\lor').replace(
+        '∧', '\\land').replace(
+        '⊕', '\\oplus').replace(
+        '¬', '\\neg')
+
+# Generates a LaTeX-formatted line
+def lt_line(l):
+    return ' & '.join(map(str,l))+'\\\\'
+
+# Generates a LaTeX-formatted header
+def lt_header(s): 
+    return lt_line(get_vars(s)+[lt_prettify(s)])
+
+# Generates a LaTeX-formatted table
+def lt_table(s):
+    return '\n'.join(map(lt_line,make_truth_table(s)))
+
+# Generates the truth table string to be saved in a LaTeX file
+def make_tt_latex(s): 
+    return "\\documentclass[a4paper]{article}\n\\begin{document}" + \
+        "\n\\title{Truth Table for $" + lt_prettify(s) + "$}\n\\author{Kyle Mueller}" + \
+        "\n\\maketitle\n\\begin{displaymath}" + \
+        "\n\\begin{array}{|" + ' '.join('c'*len(get_vars(s))) + "|c|}\n" + lt_header(s) + \
+        "\n\\hline\n" + lt_table(s) + \
+        "\n\\end{array}\n\\end{displaymath}\n\\end{document}"
 
 
 # ORG Functions
 
-org_line = lambda l: f"|{'|'.join(map(str,l))}|"
-org_header = lambda s: f"|{'|'.join(gv(s)+[prettify(s)])}|\n|{'+'.join('-'*(len(gv(s))+1))}|\n"
-org_table = lambda s: org_header(s) + '\n'.join(map(org_line,make_truth_table(s)))
-make_tt_org = lambda s: f"* Truth Table for ~{prettify(s)}~\n{org_table(s)}"
+# Generates a ORG-formatted line
+def org_line(l):
+    return f"|{'|'.join(map(str,l))}|"
+
+# Generates a ORG-formatted header
+def org_header(s):
+    vars_ = get_vars(s)
+    return f"|{'|'.join(vars_+[prettify(s)])}|\n|{'+'.join('-'*(len(vars_)+1))}|\n"
+
+# Generates a ORG-formatted table
+def org_table(s):
+    return org_header(s) + '\n'.join(map(org_line,make_truth_table(s)))
+
+# Generates the truth table string to be saved in a ORG file
+def make_tt_org(s):
+    return f"* Truth Table for ~{prettify(s)}~\n{org_table(s)}"
 
 
 # Error Checking Functions
 
-valid_s = lambda s: s.isalpha() or s in valid_symbols
-valid_bx = lambda s: all(map(valid_s,prettify(s).split())) and len(gv(s)) < 6
+# Checks if a character is valid
+def valid_c(s):
+    return s.isalpha() or s in valid_symbols
+
+# Checks if a boolean expression is valid
+def valid_bx(s):
+    return all(map(valid_c,prettify(s).split())) and len(get_vars(s)) < 6
 
 
 # Application Class
